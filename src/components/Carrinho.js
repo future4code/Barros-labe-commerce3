b97b5b1868fc3817306b8eebd83021f8f1f6e1f7
ludlulk <ludlulk@gmail.com>
2022-07-20 19:32:23 -0300

    enter

---
+++ src/components/Carrinho.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react"
+import {react } from "react"
+import { ProdAddCarrinho } from "./ProdAddCarrinho"
+
+export function Carrinho() {
+    return (
+        <Carrinho arrayProdutos={arrayProdCarrinho} setArrayProdutos={setArrayProdCarrinho}/>
+    )
+    }
+    
+    return function render() {ProdAddCarrinho}
+
+
+
+
---
+++ src/components/ProdAddCarrinho.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export function ProdAddCarrinho (props ) {
+    return(
+        <div>
+            <p> 1x </p>
+            <p> Produto: {props.nome}</p>
+            <button onClick={props.handleRemover}>Remover</button>
+        </div>
+    )
+}
