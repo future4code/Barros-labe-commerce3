775057db979c4525f46fcb9962c80f9e5a833cc7
ludlulk <ludlulk@gmail.com>
2022-07-21 20:14:29 -0300

    carrinho

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,15 +1,30 @@
-import { render } from "@testing-library/react"
-import {react } from "react"
+
+import React  from "react"
 import { ProdAddCarrinho } from "./ProdAddCarrinho"
 
-export function Carrinho() {
-    return (
-        <Carrinho arrayProdutos={arrayProdCarrinho} setArrayProdutos={setArrayProdCarrinho}/>
-    )
+import React from 'react'
+import { ProdAddCarrinho } from './ProdAddCarrinho'
+
+
+export function Carrinho(props) {
+
+
+    const handleRemover = (nomeDeletado) => {
+        const arrayAtualizado = props.arrayProdutos.filter(item => item.nome !== nomeDeletado)
+        props.setSoma(arrayAtualizado.reduce((prev, atual) => prev + (atual.preco * atual.quantidade), 0))
+        return props.setArrayProdutos(arrayAtualizado)
     }
-    
-    return function render() {ProdAddCarrinho}
 
 
-
-
+    return (
+        <section>
+            <h2>Carrinho:</h2>
+            {
+            (props.arrayProdutos.length > 0) &&
+            (props.arrayProdutos.map((item, index) => <ProdAddCarrinho key={index} nome={item.nome} quantidade={item.quantidade} handleRemover={() => handleRemover(item.nome)}/>))
+            }
+            <p>Valor Total: R${props.soma}.000,00</p>
+        </section>
+    )
+        }
+        
